c617440a76b78e792836ffed8606b65121348b37
AntonAmes <anton@lorani.de>
2021-01-16 14:47:48 +0100

    Initial Commit

---
+++ Jottre/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jottre
+//
+//  Created by Anton Lorani on 16.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
