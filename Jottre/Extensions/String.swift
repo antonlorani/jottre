2015006c3a91262c913031d369ab57eef87ae218
AntonAmes <anton@lorani.de>
2021-01-16 15:46:26 +0100

    Added documentation

--- Jottre/Extensions/String.swift
+++ Jottre/Extensions/String.swift
@@ -9,6 +9,9 @@ import UIKit
 
 extension String {
     
+    /// Computes the width of the text inside a UILabel
+    /// - Parameter label: UILabel that will contain the text
+    /// - Returns: Width in px
     func width(inside label: UILabel) -> CGFloat {
         guard let font = label.font else {
             return 0
@@ -20,6 +23,10 @@ extension String {
         return size.width
     }
     
+    
+    /// Computes the width of the text inside a UIButton
+    /// - Parameter label: UIButton that will contain the text
+    /// - Returns: Width in px
     func width(inside button: UIButton) -> CGFloat {
         guard let font = button.titleLabel?.font else {
             return 0
