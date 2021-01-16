95032aa94c837616bb8d30aa0a6666cf0a28af27
AntonAmes <anton@lorani.de>
2021-01-16 15:43:49 +0100

    Added documentation

--- Jottre/Models/NodeCollector.swift
+++ Jottre/Models/NodeCollector.swift
@@ -48,27 +48,38 @@ class NodeCollector {
 
     // MARK: - Methods
     
+    /// Loads the nodes from default path
+    /// - Parameter completion: Returns a boolean that indicates success or failure
     func pull(completion: ((Bool) -> Void)? = nil) {
         completion?(false)
     }
     
     
+    /// Pulls a specific node from url.
+    /// This node will be added to the NodeCollector
+    /// - Parameters:
+    ///   - url: Should point to a .jot file on the users file-system
+    ///   - completion: Returns a boolean that indicates success or failure
     func pullNode(url: URL, completion: ((Bool) -> Void)? = nil) {
         completion?(false)
     }
     
     
+    /// Force pulls all the Nodes to the file-system
+    /// - Parameter completion: Returns a boolean that indicates success or failure
     func push(completion: ((Bool) -> Void)? = nil) {
         completion?(false)
     }
     
     
     
+    /// Updates the meta-data for each Node in this object
     func update() {
         nodes.forEach({ $0.updateMeta() })
     }
     
     
+    /// Sends a message to each observer, that there happened changes inside this object.
     fileprivate func didUpdate() {
         
         if !observersEnabled { return }
@@ -83,16 +94,19 @@ class NodeCollector {
     
     // MARK: - Observer methods
     
+    /// Enable observer calls ;)
     func enableObservers() {
         observersEnabledValue = true
     }
     
     
+    /// Suppresses the observer calls ;)
     func disableObservers() {
         observersEnabledValue = false
     }
     
     
+    /// Adds a new observer to this class ;)
     func addObserver(_ observer: NodeCollectorObserver) {
         observers.append(observer)
     }
