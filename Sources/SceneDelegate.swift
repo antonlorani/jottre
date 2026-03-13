01d7c4745453a593be06ad574c0bd33ccf7f8f5e
Anton Lorani <28454919+antonlorani@users.noreply.github.com>
2026-03-13 22:52:25 +0100

    Initial project setup (#11)

---
+++ Sources/SceneDelegate.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else {
+            return
+        }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
