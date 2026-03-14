78d28089c713a9db6e757fa34ef8f5f87c73aaa8
Anton Lorani <28454919+antonlorani@users.noreply.github.com>
2026-03-14 21:30:31 +0100

    Fix iOS display compatibility (#13)

--- Sources/SceneDelegate.swift
+++ Sources/SceneDelegate.swift
@@ -4,6 +4,8 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    private let rootCoordinator = RootCoordinator()
+
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
@@ -12,9 +14,22 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = scene as? UIWindowScene else {
             return
         }
+
+        let url = connectionOptions.urlContexts.first?.url ?? URL(string: "https://www.jottre.com")!
+
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithDefaultBackground()
+
+        let navigationController = UINavigationController()
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.navigationBar.standardAppearance = appearance
+        navigationController.navigationBar.scrollEdgeAppearance = appearance
+        navigationController.viewControllers = rootCoordinator.handleDeepLink(url)
+
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
+        window.rootViewController = navigationController
         window.makeKeyAndVisible()
+
         self.window = window
     }
 }
