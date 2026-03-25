ef072de0e113256dec43e56f3555a25539dd7427
Anton Lorani <28454919+antonlorani@users.noreply.github.com>
2026-03-25 14:23:55 +0100

    Adopt source wide licensing checks and swift format (#18)

--- Sources/NotesPage/NoteCell/NoteCell.swift
+++ Sources/NotesPage/NoteCell/NoteCell.swift
@@ -1,3 +1,21 @@
+/*
+ Jottre: Minimalistic jotting for iPhone, iPad and Mac.
+ Copyright (C) 2021-2026 Anton Lorani
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import UIKit
 
 final class NoteCell: UICollectionViewCell, PageCell {
@@ -71,20 +89,22 @@ final class NoteCell: UICollectionViewCell, PageCell {
         contentView.addSubview(separatorLine)
         contentView.addSubview(nameLabel)
 
-        NSLayoutConstraint.activate([
-            previewImageView.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
-            previewImageView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
-            previewImageView.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
-            previewImageView.bottomAnchor.constraint(equalTo: separatorLine.topAnchor),
+        NSLayoutConstraint.activate(
+            [
+                previewImageView.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
+                previewImageView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+                previewImageView.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
+                previewImageView.bottomAnchor.constraint(equalTo: separatorLine.topAnchor),
 
-            separatorLine.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            separatorLine.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            separatorLine.heightAnchor.constraint(equalToConstant: DesignTokens.Length.separator),
+                separatorLine.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+                separatorLine.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+                separatorLine.heightAnchor.constraint(equalToConstant: DesignTokens.Length.separator),
 
-            nameLabel.topAnchor.constraint(equalTo: separatorLine.bottomAnchor, constant: DesignTokens.Spacing.sm),
-            nameLabel.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
-            nameLabel.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor)
-        ] + withoutInfoLabelConstraints)
+                nameLabel.topAnchor.constraint(equalTo: separatorLine.bottomAnchor, constant: DesignTokens.Spacing.sm),
+                nameLabel.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+                nameLabel.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
+            ] + withoutInfoLabelConstraints
+        )
     }
 
     func configure(
--- Sources/SettingsPage/SettingsViewModel.swift
+++ Sources/SettingsPage/SettingsViewModel.swift
@@ -1,3 +1,21 @@
+/*
+ Jottre: Minimalistic jotting for iPhone, iPad and Mac.
+ Copyright (C) 2021-2026 Anton Lorani
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import UIKit
 
 @MainActor
@@ -39,10 +57,10 @@ final class SettingsViewModel: PageViewModel {
                 },
                 onAction: { _ in }
             ),
-//            .settingsToggle(
-//                name: "iCloud Synchronization",
-//                isOn: false
-//            ),
+            //            .settingsToggle(
+            //                name: "iCloud Synchronization",
+            //                isOn: false
+            //            ),
             .settingsExternalLink(
                 name: L10n.Settings.ICloud.title,
                 info: L10n.Settings.ICloud.info,
@@ -64,7 +82,7 @@ final class SettingsViewModel: PageViewModel {
             .settingsInfo(
                 name: L10n.Settings.Version.title,
                 value: "2.0.0"
-            )
+            ),
         ])
 
         rightNavigationItemsContinuation.yield([
