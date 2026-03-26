39a4f4040a8f621fc83a14c3f0f9f36d50fb635a
Anton Lorani <28454919+antonlorani@users.noreply.github.com>
2026-03-26 22:53:02 +0100

    Makes cloud related file system API asynchronus (#23) * Can block the current thread if the apps cloud container is created

--- Sources/FileService.swift
+++ Sources/FileService.swift
@@ -1,10 +1,28 @@
+/*
+ Jottre: Minimalistic jotting for iPhone, iPad and Mac.
+ Copyright (C) 2021-2026 Anton Lorani
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import Foundation
 
 protocol FileServiceProtocol: Sendable {
 
     func localDocumentsDirectory() throws -> URL?
 
-    func cloudDocumentsDirectory() throws -> URL?
+    func cloudDocumentsDirectory() async throws -> URL?
 
     func listContents(
         directory: URL,
@@ -40,7 +58,7 @@ struct FileService: FileServiceProtocol {
         return directory
     }
 
-    func cloudDocumentsDirectory() throws -> URL? {
+    func cloudDocumentsDirectory() async throws -> URL? {
         guard let directory = fileManager.url(forUbiquityContainerIdentifier: nil) else {
             return nil
         }
